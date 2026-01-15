ab197e5c34c16899fdd01b6a8f416ccef5246ba9
ntorh <ngcobontombenhle434@gmail.com>
2026-01-15 14:09:25 +0200

    configure firebase auth and firestore

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWSYnW3zLk8mJ6LBM89iCvHxwOrpYgsU4",
+  authDomain: "symmetric-blade-466812-s3.firebaseapp.com",
+  projectId: "symmetric-blade-466812-s3",
+  storageBucket: "symmetric-blade-466812-s3.firebasestorage.app",
+  messagingSenderId: "1038634868705",
+  appId: "1:1038634868705:web:e30d3d910bf6a79b873ca0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
